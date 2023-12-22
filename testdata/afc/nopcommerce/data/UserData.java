2270e3a6f03a2bc39ac48ed324f5784a43e20486
Do Duy Dong <doduydong.d1310@gmail.com>
2023-12-22 11:22:39 +0700

    Manage Data - Internal File

--- testdata/afc/nopcommerce/data/UserData.java
+++ testdata/afc/nopcommerce/data/UserData.java
@@ -2,4 +2,12 @@ package afc.nopcommerce.data;
 
 public class UserData {
 
+	public static class UserInfo {
+		public static final String FIRST_NAME = "Duy Dong";
+		public static final String LAST_NAME = "Do";
+		public static final String EMAIL_USER_NAME = "dongafc";
+		public static final String EMAIL_DOMAIN = "@gmail.com";
+		public static final String PASSWORD = "Selenium3";
+	}
+
 }
